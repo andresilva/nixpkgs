45a36e22de97a26a741e1c0f0799576aa8ad5276
André Silva <andrerfosilva@gmail.com>
2021-07-16 22:37:05 +0100

    pure-prompt: 1.17.0 -> 1.16.0 (downgrade)

--- pkgs/shells/zsh/pure-prompt/default.nix
+++ pkgs/shells/zsh/pure-prompt/default.nix
@@ -4,13 +4,13 @@ with lib;
 
 stdenv.mkDerivation rec {
   pname = "pure-prompt";
-  version = "1.17.0";
+  version = "1.16.0";
 
   src = fetchFromGitHub {
     owner = "sindresorhus";
     repo = "pure";
     rev = "v${version}";
-    sha256 = "sha256-6j6QZtsA5ZgfXthYjXRrND2zAJwZx0/6WRI1f3c+2mE=";
+    sha256 = "1wsmv32pdcs0y5xq4537v66bijgnblj04bqa2k2pwja0nja3hyby";
   };
 
   installPhase = ''
