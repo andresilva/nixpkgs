fb508bb70737ea9556d5437a2f2587b7b9f6f891
André Silva <andrerfosilva@gmail.com>
2024-09-24 10:57:00 +0100

    signal-desktop: allow building with original upstream emojis

--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop.nix
@@ -1,8 +1,9 @@
-{ callPackage }:
-callPackage ./generic.nix { } rec {
+{ callPackage, replaceEmojis }:
+callPackage ./generic.nix { inherit replaceEmojis; } rec {
   pname = "signal-desktop";
   dir = "Signal";
   version = "7.29.0";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_${version}_amd64.deb";
   hash = "sha256-TftTt9pN4EjgZs3N94TlCzXK64g1g+oGPx1ZLVX02VA=";
+  hash-original-emojis = "sha256-MyoCPIrKidaPO20hEHekSWbC3FxROVPmu/KDwMgYypA=";
 }
