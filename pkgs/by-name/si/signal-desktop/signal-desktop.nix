5df1bf74b2e1618a16b99c0f7b2d82b8871bf919
André Silva <andrerfosilva@gmail.com>
2025-02-07 15:04:18 +0000

    signal-desktop: don't replace upstream emojis by default

--- pkgs/by-name/si/signal-desktop/signal-desktop.nix
+++ pkgs/by-name/si/signal-desktop/signal-desktop.nix
@@ -4,5 +4,5 @@ callPackage ./generic.nix { } rec {
   dir = "Signal";
   version = "7.46.0";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-  hash = "sha256-HbmyivfhvZfXdtcL/Cjzl4v0Ck/fJCD517iTjIeidgc=";
+  hash = "sha256-ljylBEeLpGJ1w8PjJtvvcRAW8NZv0qgQR40BRGuUpIc=";
 }
